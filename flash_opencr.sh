1c172c7fca105dd6698440ee5d7e452cf1a6b608
NiklasPerssonMDU <niklas.persson@mdu.se>
2026-04-28 13:11:49 +0200

    add: flash_opencr.sh för OpenCR-firmware på TurtleBot3 Burger

---
+++ flash_opencr.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Flashar OpenCR-kortet (motorstyrkortet) med TurtleBot3 Burger-firmware.
+# Kör på roboten med OpenCR ansluten via USB (/dev/ttyACM0).
+set -e
+
+OPENCR_PORT=/dev/ttyACM0
+OPENCR_MODEL=burger
+
+echo "--- Flashar OpenCR för TurtleBot3 $OPENCR_MODEL ---"
+echo "Port: $OPENCR_PORT"
+
+if [ ! -c "$OPENCR_PORT" ]; then
+    echo "FEL: $OPENCR_PORT finns inte. Kontrollera att OpenCR är ansluten via USB."
+    exit 1
+fi
+
+cd /tmp
+rm -rf opencr_update.tar.bz2 opencr_update
+wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS2/latest/opencr_update.tar.bz2
+tar xvf opencr_update.tar.bz2
+cd opencr_update && ./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr
+
+echo ""
+echo "--- OpenCR flashad! Starta om roboten och kör bringup. ---"
